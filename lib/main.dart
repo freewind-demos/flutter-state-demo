356d9f86e0861fc87b9587382ddc61160bd032f6
Freewind <nowindlee@gmail.com>
2026-03-14 22:11:17 +0800

    flutter-state-demo

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+// Flutter 状态管理
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Count: $_counter'),
+              ElevatedButton(
+                onPressed: _increment,
+                child: Text('Increment'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
